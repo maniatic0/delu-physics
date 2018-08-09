5f9036416404fbddd3e26983cf4ffb6e42ba1a8b
Christian Oliveros <christianol_01@hotmail.com>
2018-08-09 19:35:31 -0400

    Gravity and Movement

---
+++ DELU Physics/Assets/Scripts/GravityController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravityController : MonoBehaviour {
+
+	public float CurrentGravityModifier{get; private set;}
+
+	public float CurrentGravityModifierNormalized{get; private set;}
+
+	[SerializeField]
+	private float minGrav = 0.3f;
+
+	[SerializeField]
+	private float maxGrav = 2f;
+
+	[SerializeField]
+	private float gravMaxStep = 0.01f;
+
+	private float gravity = -9.81f;
+	// Use this for initialization
+
+	private Vector3 grav;
+
+	private void Awake() {
+		gravity = Physics.gravity.y;
+		CurrentGravityModifier = 1f;
+		CurrentGravityModifierNormalized = (CurrentGravityModifier - minGrav) / maxGrav;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ChangeGravity();
+		Debug.Log("Gravity: " + CurrentGravityModifierNormalized.ToString());
+	}
+
+	void ChangeGravity() {
+		float change = gravMaxStep * Input.GetAxis("Vertical") * Time.deltaTime;
+		CurrentGravityModifier = Mathf.Clamp(
+			CurrentGravityModifier + change, 
+			minGrav, 
+			maxGrav
+		);
+		CurrentGravityModifierNormalized = (CurrentGravityModifier - minGrav) / maxGrav;
+		grav.y = gravity * CurrentGravityModifier;
+		Physics.gravity = grav; 
+	}
+}
---
+++ DELU Physics/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(GravityController))]
+public class PlayerMovement : MonoBehaviour {
+
+	private Rigidbody body;
+
+	private GravityController grav;
+
+	[SerializeField]
+	private float minAcc = 5f;
+
+	[SerializeField]
+	private float maxAcc = 10f;
+	// Use this for initialization
+
+	private float currentAcc = 0f;
+	private void Awake() {
+		body = GetComponent<Rigidbody>();
+		grav = GetComponent<GravityController>();
+	}
+	void Start () {
+		currentAcc = Mathf.Lerp(minAcc, maxAcc, grav.CurrentGravityModifierNormalized);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentAcc = Mathf.Lerp(minAcc, maxAcc, grav.CurrentGravityModifierNormalized);
+		body.AddForce(currentAcc * Vector3.right, ForceMode.Force);
+		Debug.Log("Acceleration: " + currentAcc.ToString());
+	}
+}
