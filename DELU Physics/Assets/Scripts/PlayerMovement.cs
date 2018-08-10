2975680c9f8d43a6e10e1718a410664b67caf7fe
Carlos Infante <carlosinfante1220@gmail.com>
2018-08-10 15:52:28 -0400

    (I kinda forgot about the change from accel to vel)

--- DELU Physics/Assets/Scripts/PlayerMovement.cs
+++ DELU Physics/Assets/Scripts/PlayerMovement.cs
@@ -11,25 +11,26 @@ public class PlayerMovement : MonoBehaviour {
 	private GravityController grav;
 
 	[SerializeField]
-	private float minAcc = 5f;
+	private float minVel = 5f;
 
 	[SerializeField]
-	private float maxAcc = 10f;
+	private float maxVel = 10f;
 	// Use this for initialization
 
-	private float currentAcc = 0f;
+	private float currentVel = 0f;
 	private void Awake() {
 		body = GetComponent<Rigidbody>();
 		grav = GetComponent<GravityController>();
 	}
 	void Start () {
-		currentAcc = Mathf.Lerp(minAcc, maxAcc, grav.CurrentGravityModifierNormalized);
+		currentVel = Mathf.Lerp(minVel, maxVel, grav.CurrentGravityModifierNormalized);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		currentAcc = Mathf.Lerp(minAcc, maxAcc, grav.CurrentGravityModifierNormalized);
-		body.AddForce(currentAcc * Vector3.right, ForceMode.Force);
-		Debug.Log("Acceleration: " + currentAcc.ToString());
+		currentVel = Mathf.Lerp(minVel, maxVel, grav.CurrentGravityModifierNormalized);
+		//body.AddForce(currentAcc * Vector3.right, ForceMode.Force);
+        body.velocity = new Vector3(currentVel, body.velocity.y, body.velocity.z);
+		Debug.Log("Acceleration: " + currentVel.ToString());
 	}
 }
